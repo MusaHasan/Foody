55fb16fe3d2d476556d50b75c58746f1767bd84a
MusaHasan <musahasan17750@gmail.com>
2023-08-29 19:32:17 +0600

    Nav bar done, Home section in progress

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-let menu = document.querySelector('#manu-bars');
+let menu = document.querySelector('#manu-bars'); // For manue bar on (max-width: 768px) start
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-    return;
+} 
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active'); // For manue bar on (max-width: 768px) end
+} 
+
+document.querySelector('#search-icon').onclick = () =>{         // For Search system start
+    document.querySelector('#search-form').classList.toggle('active')
+}
+
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove ('active') // For Search system end
 }
