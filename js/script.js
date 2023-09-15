c1a4ff0e2f98327a38a8674a8444de8d470fef21
MusaHasan <musahasan17750@gmail.com>
2023-09-15 23:26:51 +0600

    Review saction design complete

--- js/script.js
+++ js/script.js
@@ -1,20 +1,63 @@
-let menu = document.querySelector('#manu-bars'); // For manue bar on (max-width: 768px) start
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#manu-bars"); // For manue bar on (max-width: 768px) start
+let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-} 
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active'); // For manue bar on (max-width: 768px) end
-} 
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active"); // For manue bar on (max-width: 768px) end
+};
 
-document.querySelector('#search-icon').onclick = () =>{         // For Search system start
-    document.querySelector('#search-form').classList.toggle('active')
-}
+document.querySelector("#search-icon").onclick = () => {
+  // For Search system start
+  document.querySelector("#search-form").classList.toggle("active");
+};
 
-document.querySelector('#close').onclick = () =>{
-    document.querySelector('#search-form').classList.remove ('active') // For Search system end
-}
+document.querySelector("#close").onclick = () => {
+  document.querySelector("#search-form").classList.remove("active"); // For Search system end
+};
+
+// Initialize Swiper
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1080: {
+      slidesPerView: 3,
+    },
+  },
+}); // Initialize Swiper end
